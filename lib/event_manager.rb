9d0d38ea3da5a624cb2e4d1ffa92fc7c7f4969df
Meg Peer <megimu@gmail.com>
2024-08-27 08:52:39 -0400

    atendee phone number cleanup method working

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -6,6 +6,17 @@ def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5,"0")[0..4]
 end
 
+def clean_phone_number(phone_number)
+ phone_number.tr!('^0-9', '')
+ phone_number.delete_prefix("1")
+
+ if phone_number.length == 10
+  return phone_number
+ else
+  phone_number = "N/A"
+ end
+end
+
 def legislators_by_zipcode(zip)
   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
   civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
@@ -21,15 +32,15 @@ def legislators_by_zipcode(zip)
   end
 end
 
-def save_thank_you_letter(id,form_letter)
-  Dir.mkdir('output') unless Dir.exist?('output')
+# def save_thank_you_letter(id,form_letter)
+#   Dir.mkdir('output') unless Dir.exist?('output')
 
-  filename = "output/thanks_#{id}.html"
+#   filename = "output/thanks_#{id}.html"
 
-  File.open(filename, 'w') do |file|
-    file.puts form_letter
-  end
-end
+#   File.open(filename, 'w') do |file|
+#     file.puts form_letter
+#   end
+# end
 
 puts 'EventManager initialized.'
 
@@ -39,16 +50,19 @@ contents = CSV.open(
   header_converters: :symbol
 )
 
-template_letter = File.read('form_letter.erb')
-erb_template = ERB.new template_letter
+# template_letter = File.read('form_letter.erb')
+# erb_template = ERB.new template_letter
 
 contents.each do |row|
   id = row[0]
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
+  phone_number = clean_phone_number(row[:homephone])
   legislators = legislators_by_zipcode(zipcode)
 
-  form_letter = erb_template.result(binding)
+#   form_letter = erb_template.result(binding)
 
-  save_thank_you_letter(id,form_letter)
+#   save_thank_you_letter(id,form_letter)
+
+puts "#{name} #{zipcode} #{phone_number}"
 end
