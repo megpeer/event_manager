01d5c55136469bcd61ba27bf7481036b71896e15
Meg Peer <megimu@gmail.com>
2024-08-30 08:10:36 -0400

    return ascending order hash of hours, with number of people who registered per hour

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,6 +1,9 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
 require 'erb'
+require 'time'
+
+
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5,"0")[0..4]
@@ -42,7 +45,18 @@ end
 #   end
 # end
 
+def hours(reg_date)
+  time = Time.strptime(reg_date, '%m/%d/%Y %k:%M')
+  reg_date = time.hour.to_int
+  @timehash[reg_date] += 1
+end
+
+
 puts 'EventManager initialized.'
+
+@timehash = {}
+ary = (0..23).to_a
+ary.each{|a| @timehash[a] = 0}
 
 contents = CSV.open(
   'event_attendees.csv',
@@ -58,11 +72,18 @@ contents.each do |row|
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
   phone_number = clean_phone_number(row[:homephone])
+  reg_date = hours(row[:regdate])
   legislators = legislators_by_zipcode(zipcode)
+
+
+
 
 #   form_letter = erb_template.result(binding)
 
 #   save_thank_you_letter(id,form_letter)
 
 puts "#{name} #{zipcode} #{phone_number}"
+
 end
+sorted_timehash = @timehash.sort_by {|_key, value| value}.to_h
+puts sorted_timehash
